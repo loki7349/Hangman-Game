43c3c0504c82c92e360c8ee773a57ab3ba66756d
loki7349 <lokeshargon19l@gmail.com>
2024-01-16 00:12:50 +0530

    added the eventListener

---
+++ script/script.js
@@ -0,0 +1,18 @@
+const wordDisplay = document.querySelector('.word-display')
+const keyboardDiv = document.querySelector('.keyboard')
+
+const getRandomWord = () => {
+    
+    const {word, hint} = wordList[Math.floor(Math.random() * wordList.length)];
+    document.querySelector(".hint-text b").innerText = hint;
+    wordDisplay.innerHTML = word.split("").map(()=> `<li class="letter"></li>`).join("")
+}
+
+for (let i = 97; i <= 122; i++) {
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i)
+    keyboardDiv.appendChild(button)
+    button.addEventListener("click", e => initGame(e.target.fromCharCode(i)))
+}
+
+getRandomWord();
