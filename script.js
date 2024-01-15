3e2bdfc9992cf9fe7ecb2c58a0b20b2df57d56d4
loki7349 <lokeshargon19l@gmail.com>
2024-01-15 23:48:00 +0530

    add the word list for the game

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const keyboardDiv = document.querySelector('.keyboard')
 
+const getrandomWord = () => {
+    
+}
+
 for (let i = 97; i <= 122; i++) {
     const buttton = document.createElement("button");
     buttton.innerText = String.fromCharCode(i)
